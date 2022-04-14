2f12a96f67e50d06e9bd1b88704e2b403dd5b63a
Paolo <p.sguazz@gmail.com>
2022-04-14 13:30:09 +0200

    Cleaning up manifest

--- lib/bridgetown-dato.rb
+++ lib/bridgetown-dato.rb
@@ -2,10 +2,8 @@
 
 require "bridgetown"
 require "bridgetown-dato/builder"
+require "bridgetown-dato/document"
 
 Bridgetown::PluginManager.new_source_manifest(
   origin: BridgetownDato,
-  components: File.expand_path("../components", __dir__),
-  content: File.expand_path("../content", __dir__),
-  layouts: File.expand_path("../layouts", __dir__)
 )
