3f11824289400ff3e66c2c6ba8fe00887add5880
Paolo <p.sguazz@gmail.com>
2022-04-15 10:38:44 +0200

    Optional meta fields
    Defining them by default doesn't work well with the schema, because they get filed under the wrong name.

--- lib/bridgetown-dato/model.rb
+++ lib/bridgetown-dato/model.rb
@@ -14,14 +14,6 @@ module BridgetownDato
       BridgetownDato::Schema.fields(model_name)
     end
 
-    def self.singleton!
-      @singleton = true
-    end
-
-    def self.singleton?
-      @singleton || false
-    end
-
     def self.field(key, type: :markdown, path: key)
       BridgetownDato::Schema.add_field(model_name, key)
 
@@ -36,15 +28,25 @@ module BridgetownDato
       end
     end
 
-    field :meta_created_at, path: %w[meta created_at]
-    field :meta_current_version, path: %w[meta current_version]
-    field :meta_first_published_at, path: %w[meta first_published_at]
-    field :meta_is_valid, path: %w[meta is_valid]
-    field :meta_publication_scheduled_at, path: %w[meta publication_scheduled_at]
-    field :meta_published_at, path: %w[meta published_at]
-    field :meta_status, path: %w[meta status]
-    field :meta_unpublishing_scheduled_at, path: %w[meta unpublishing_scheduled_at]
-    field :meta_updated_at, path: %w[meta updated_at]
+    def self.meta_fields!
+      field :meta_created_at, path: %w[meta created_at]
+      field :meta_current_version, path: %w[meta current_version]
+      field :meta_first_published_at, path: %w[meta first_published_at]
+      field :meta_is_valid, path: %w[meta is_valid]
+      field :meta_publication_scheduled_at, path: %w[meta publication_scheduled_at]
+      field :meta_published_at, path: %w[meta published_at]
+      field :meta_status, path: %w[meta status]
+      field :meta_unpublishing_scheduled_at, path: %w[meta unpublishing_scheduled_at]
+      field :meta_updated_at, path: %w[meta updated_at]
+    end
+
+    def self.singleton!
+      @singleton = true
+    end
+
+    def self.singleton?
+      @singleton || false
+    end
 
     def initialize(raw_model)
       self.raw_model = raw_model
